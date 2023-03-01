5f62d84e5563525be80356b61052db0fb76451a6
Romron <romron@ukr.net>
2023-03-01 16:31:16 +0200

    что то

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# e-shop-clothes
-Another one e-shop clothes
+# gulp-assembly-1.0
+
+алгоритм 
+1. Скачать шрифты в папку fonts
+2. Сгенерировать шрифты:  npm run createFonts
+3. Сгенерировать сетку:   npm run grid
--- dist/index.php
+++ dist/index.php
@@ -6,7 +6,7 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-   <link href="./styles/style.min.css?_v=20230227154629" rel="stylesheet" type="text/css">
+   <link href="./styles/style.min.css" rel="stylesheet" type="text/css">
 
    <title>Document</title>
 
@@ -14,22 +14,13 @@
 
 <body>
 
-   <div class="ss"> Привет </div>
-
-   <div>
-
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis cupiditate, ducimus non, rerum qui architecto ullam blanditiis, ab sit modi error, inventore repudiandae quisquam ipsa? Molestiae nobis ut voluptatibus dicta.
-
-   </div>
-
-   <div class="background"></div>
-
-   <div class="img">
-      <picture><source srcset="img/some_img.webp" type="image/webp"><img src="img/some_img.jpg" alt=""></picture>
-   </div>
+   иконки
+   FontAwesome
+   Simple-Line-Icons
 
 
-   <script src="js/app.min.js?_v=20230227154629"></script>
+
+   <script src="js/app.min.js"></script>
 </body>
 
 
