0864f1b107a615f0ece3e536945c38251079c12c
Romron <romron@ukr.net>
2023-03-02 16:38:51 +0200

    сктка и основные блоки

--- dist/index.php
+++ dist/index.php
@@ -14,7 +14,15 @@
 
 <body>
    <div class="header conteiner"></div>
-   <div class="conteiner" id="dish">
+   <div class="background">
+      <div class="conteiner" id="dish">
+         <div class="dish-title">
+            <p class="dish-title__title">Teast your fav dish</p>
+            <p class="dish-title__subtitle-title">from <span>luxury restaurent.</span></p>
+         </div>
+      </div>
+   </div>
+   <div class="conteiner" id="about">
       <div class="row">
          <div class="item"></div>
          <div class="item"></div>
@@ -23,9 +31,6 @@
          <div class="item"></div>
          <div class="item"></div>
       </div>
-   </div>
-   <div class="conteiner" id="about">
-      <!-- <div class="row"></div> -->
    </div>
    <div class="conteiner" id="today">
       <!-- <div class="row"></div> -->
