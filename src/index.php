6e896669a1d9ab5cfd49afa37a4d111792a6cd09
Romron <romron@ukr.net>
2023-03-03 16:22:30 +0200

    header start

--- src/index.php
+++ src/index.php
@@ -13,12 +13,39 @@
 </head>
 
 <body>
-   <div class="header conteiner"></div>
-   <div class="background">
+   <nav class="conteiner">
+      <div class="header ">
+         <a href="#" class="logo"><img src="./img/logo.png" alt="logo"></a>
+
+         <nav class="heder-menu">
+            <ul>
+               <li><a href="#">home</a></li>
+               <li><a href="#">menu</a></li>
+               <li><a href="#">reservation</a></li>
+               <li><a href="#">recipe</a></li>
+               <li><a href="#">blog</a></li>
+               <li><a href="#">pages</a></li>
+               <li><a href="#">contact</a></li>
+               <li><a href="#">cart</a></li>
+            </ul>
+         </nav>
+         <div class="heder-menu__button"><a href="#">call back</a></div>
+
+
+
+      </div>
+
+   </nav>
+   <div class="background background__dish">
       <div class="conteiner" id="dish">
          <div class="dish-title">
             <p class="dish-title__title">Teast your fav dish</p>
             <p class="dish-title__subtitle-title">from <span>luxury restaurent.</span></p>
+         </div>
+         <div class="img-text">Image <br> 1970 x 570</div>
+         <div class="exlore-food-menu">
+            <img src="./img/exlore-food-menu.png" alt="exlore-food-menu.jpg">
+            <p>Exlore food Menu</p>
          </div>
       </div>
    </div>
