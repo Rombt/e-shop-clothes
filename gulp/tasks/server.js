04959c5e65360377667fbbdf09f0de021790ce45
roman <romron@ukr.net>
2023-03-10 21:30:02 +0200

    menu burger

--- gulp/tasks/server.js
+++ gulp/tasks/server.js
@@ -8,6 +8,7 @@ export const server = (done) => {
     let rootFolder = app.path.prodFolder.slice(2);
     let proxyPath = '';
 
+
     if (absPath.includes('OSPanel')) {
         proxyPath = `http://rombt/${app.path.rootFolder}/${rootFolder}`; // домашний комп
     } else {
--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,20 @@
-import { isWebp } from "./modules/isWebp.js";     // проверяет поддерживает ли браузер формат webp
+import { isWebp } from "./modules/isWebp.js"; // проверяет поддерживает ли браузер формат webp
 
 
 
 isWebp();
+
+
+
+let burgerMenu = document.querySelector('.menu__burger');
+let hederMenu = document.querySelector('.heder-menu');
+
+
+
+burgerMenu.addEventListener("click", () => {
+
+    // hederMenu.style.display = 'block';
+    hederMenu.classList.toggle('menu__burger-active');
+
+
+});;
