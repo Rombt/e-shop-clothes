7b15d70b161308314ef159877201d45f05dd2116
roman <romron@ukr.net>
2023-03-13 20:10:14 +0200

    swiper работает

--- src/js/app.js
+++ src/js/app.js
@@ -12,3 +12,30 @@ burgerMenu.addEventListener("click", () => {
     hederMenu.classList.toggle('menu__sm');
     burgerMenu.classList.toggle('menu__burger-close');
 });;
+
+
+const swiper = new Swiper('.swiper', {
+    direction: 'vertical',
+    loop: true,
+
+    autoplay: {
+        delay: 5000,
+    },
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true,
+    },
+
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    // navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    // },
+
+});
