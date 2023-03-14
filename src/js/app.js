eeaab6ec50c98dc1286a4cb53223aa0b7f2d4b05
Rombt <rombt@gmail.com>
2023-03-14 13:18:14 +0200

    first swiper is ready

--- src/js/app.js
+++ src/js/app.js
@@ -25,17 +25,14 @@ const swiper = new Swiper('.swiper', {
     fadeEffect: {
         crossFade: true,
     },
-
-
-    // If we need pagination
     pagination: {
         el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">  <span class="swiper-pagination__center"> </span> </span>';
+        },
     },
-
-    // Navigation arrows
-    // navigation: {
-    //     nextEl: '.swiper-button-next',
-    //     prevEl: '.swiper-button-prev',
-    // },
-
+    mousewheel: {
+        invert: true,
+    },
 });
