41d1c121bf13f4ce9dbe4cc1849a1241949d45f0
roman <romron@ukr.net>
2023-03-20 21:03:57 +0200

    класс burger

--- src/js/app.js
+++ src/js/app.js
@@ -1,34 +1,58 @@
 import { isWebp } from "./modules/isWebp.js"; // проверяет поддерживает ли браузер формат webp
 import { swiperDish, swiperToday, swiperClients } from "./modules/sliders.js"; // проверяет поддерживает ли браузер формат webp
+import { Burger } from "./modules/burger.js";
 
-window.onload = function () {
+
+
+window.onload = function() {
 
 
     isWebp(); // проверяет поддерживает ли браузер формат webp
 
+    let params_headerMenuBurger = {
 
-    burgerMenu('heder-menu__burger', 'heder-menu', 'heder-menu__burger-close', 'menu__sm');       // burger_menu header
-    burgerMenu('food-menu-burger', 'food-menu', 'heder-menu__burger-close', 'active');       // burger_menu food-menu
-
-
-    function burgerMenu(menuBurger, menu, menuBurgerClose, active) {
-
-        menuBurger = '.' + menuBurger;
-        menu = '.' + menu;
-
-        let burgerMenu = document.querySelector(menuBurger);
-        let hederMenu = document.querySelector(menu);
-
-        console.log("burgerMenu = ", burgerMenu);
-        // console.log("hederMenu = ", hederMenu);
-
-        burgerMenu.addEventListener("click", () => {
-
-            hederMenu.classList.toggle(active);
-            burgerMenu.classList.toggle(menuBurgerClose);
-        });;
+        conteiner: 'heder-menu',
+        menuList: 'menu__list',
+        classActive: 'menu__sm',
+        burgerOpen: 'heder-menu__burger',
+        burgerClose: 'heder-menu__burger-close',
 
     }
+
+    let params_foodMenuBurger = {
+
+        conteiner: 'menu',
+        menuList: 'menu__list',
+        classActive: 'active',
+        burgerOpen: 'menu__burger',
+        burgerClose: 'burger__close',
+
+    }
+
+    const headerMenuBurger = new Burger(params_headerMenuBurger);
+    headerMenuBurger.run();
+
+    const foodMenuBurger = new Burger(params_foodMenuBurger);
+    foodMenuBurger.run();
+
+    // burgerMenu('heder-menu__burger', 'heder-menu', 'heder-menu__burger-close', 'menu__sm'); // burger_menu header
+    // burgerMenu('food-menu-burger', 'food-menu', 'heder-menu__burger-close', 'active'); // burger_menu food-menu
+
+
+    // function burgerMenu(menuBurger, menu, menuBurgerClose, active) {
+
+    //     menuBurger = '.' + menuBurger;
+    //     menu = '.' + menu;
+
+    //     let burgerMenu = document.querySelector(menuBurger);
+    //     let hederMenu = document.querySelector(menu);
+    //     burgerMenu.addEventListener("click", () => {
+
+    //         hederMenu.classList.toggle(active);
+    //         burgerMenu.classList.toggle(menuBurgerClose);
+    //     });;
+
+    // }
 
 
 
