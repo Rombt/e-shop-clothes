39e2f0f82a12cc0656f076e0036874d5b7de529b
Rombt <rombt@gmail.com>
2023-03-21 11:11:45 +0200

    подключенин меню бургер через класс

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,10 @@
 import { isWebp } from "./modules/isWebp.js"; // проверяет поддерживает ли браузер формат webp
-import { swiperDish, swiperToday, swiperClients } from "./modules/sliders.js"; // проверяет поддерживает ли браузер формат webp
+import { swiperDish, swiperToday, swiperClients } from "./modules/sliders.js";
 import { Burger } from "./modules/burger.js";
 
 
 
-window.onload = function() {
+window.onload = function () {
 
 
     isWebp(); // проверяет поддерживает ли браузер формат webp
@@ -21,38 +21,21 @@ window.onload = function() {
 
     let params_foodMenuBurger = {
 
-        conteiner: 'menu',
-        menuList: 'menu__list',
+        conteiner: 'food-menu',
+        menuList: 'food-menu__list',
         classActive: 'active',
-        burgerOpen: 'menu__burger',
+        burgerOpen: 'food-menu-burger',
         burgerClose: 'burger__close',
 
     }
 
+
     const headerMenuBurger = new Burger(params_headerMenuBurger);
-    headerMenuBurger.run();
+    headerMenuBurger.init();
 
     const foodMenuBurger = new Burger(params_foodMenuBurger);
-    foodMenuBurger.run();
+    foodMenuBurger.init();
 
-    // burgerMenu('heder-menu__burger', 'heder-menu', 'heder-menu__burger-close', 'menu__sm'); // burger_menu header
-    // burgerMenu('food-menu-burger', 'food-menu', 'heder-menu__burger-close', 'active'); // burger_menu food-menu
-
-
-    // function burgerMenu(menuBurger, menu, menuBurgerClose, active) {
-
-    //     menuBurger = '.' + menuBurger;
-    //     menu = '.' + menu;
-
-    //     let burgerMenu = document.querySelector(menuBurger);
-    //     let hederMenu = document.querySelector(menu);
-    //     burgerMenu.addEventListener("click", () => {
-
-    //         hederMenu.classList.toggle(active);
-    //         burgerMenu.classList.toggle(menuBurgerClose);
-    //     });;
-
-    // }
 
 
 
