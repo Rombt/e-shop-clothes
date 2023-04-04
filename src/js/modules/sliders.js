3a73f7333ee0fa57cc96d0e000fba2bee16526ff
roman <romron@ukr.net>
2023-04-04 19:09:41 +0300

    что то

--- src/js/modules/sliders.js
+++ src/js/modules/sliders.js
@@ -1,27 +1,30 @@
 export const swiperCoocs = new Swiper('.swiper-coocs', {
 
-   direction: "horizontal",
-   // freeMode: true,
+    direction: "horizontal",
+    // freeMode: true,
 
-   pagination: {
-      el: '.swiper-pagination',
-      // el: '.swiper-coocs__pagination',
-      type: 'bullets',
-   },
+    pagination: {
+        // el: '.swiper-pagination',
+        el: '.swiper-coocs__pagination',
+        type: 'bullets',
 
+        clickable: true,
 
-   // mousewheel: true,
-   // speed: 500,
-   // autoplay: {
-   //    delay: 5000,
-   // },
+    },
 
 
-   slidesPerView: 1,
-   spaceBetween: 10,
-   pagination: {
-      clickable: true,
-   },
+    // mousewheel: true,
+    // speed: 500,
+    // autoplay: {
+    //    delay: 5000,
+    // },
+
+
+    slidesPerView: 1,
+    spaceBetween: 23,
+    // pagination: {
+    //     clickable: true,
+    // },
 
 });
 
@@ -92,91 +95,90 @@ export const swiperCoocs = new Swiper('.swiper-coocs', {
 
 
 export const swiperDish = new Swiper('.swiper.swiper-dish', {
-   direction: 'horizontal',
-   speed: 1000,
+    direction: 'horizontal',
+    speed: 1000,
 
 
-   breakpoints: {
-      1250: {
-         direction: 'vertical',
-      }
-   },
+    breakpoints: {
+        1250: {
+            direction: 'vertical',
+        }
+    },
 
-   loop: true,
-   autoplay: {
-      delay: 5000,
-   },
-   effect: 'fade',
-   fadeEffect: {
-      crossFade: true,
-   },
-   pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      renderBullet: function (index, className) {
-         return '<span class="' + className + '">  <span class="swiper-pagination__center"> </span> </span>';
-      },
-   },
-   // mousewheel: {
-   //    invert: true,
-   // },
+    loop: true,
+    autoplay: {
+        delay: 5000,
+    },
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function(index, className) {
+            return '<span class="' + className + '">  <span class="swiper-pagination__center"> </span> </span>';
+        },
+    },
+    // mousewheel: {
+    //    invert: true,
+    // },
 });
 
 export const swiperToday = new Swiper('.swiper.swiper-today', {
-   direction: 'horizontal',
-   loop: true,
-   speed: 1000,
+    direction: 'horizontal',
+    loop: true,
+    speed: 1000,
 
-   autoplay: {
-      delay: 5000,
-   },
-   effect: 'fade',
-   fadeEffect: {
-      crossFade: true,
-   },
-   pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      renderBullet: function (index, className) {
-         return '<span class="' + className + '">  <span class="swiper-pagination__center"> </span> </span>';
-      },
-   },
-   // mousewheel: {
-   //    invert: true,
-   // },
+    autoplay: {
+        delay: 5000,
+    },
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function(index, className) {
+            return '<span class="' + className + '">  <span class="swiper-pagination__center"> </span> </span>';
+        },
+    },
+    // mousewheel: {
+    //    invert: true,
+    // },
 });
 
 
 export const swiperClients = new Swiper('.swiper.swiper-clients', {
-   loop: true,
-   speed: 500,
+    loop: true,
+    speed: 500,
 
-   autoplay: {
-      delay: 5000,
-   },
+    autoplay: {
+        delay: 5000,
+    },
 
-   navigation: {
-      nextEl: '.swiper-clients__button-prev',
-      prevEl: '.swiper-clients__button-next',
-   },
+    navigation: {
+        nextEl: '.swiper-clients__button-prev',
+        prevEl: '.swiper-clients__button-next',
+    },
 
 });
 
 
 export const swiperFoodStep = new Swiper('.swiper-food-step', {
 
-   direction: "vertical",
-   slidesPerView: "auto",
-   freeMode: true,
-   scrollbar: {
-      el: ".swiper-scrollbar",
-      draggable: true,
-   },
-   mousewheel: true,
-   speed: 500,
-   autoplay: {
-      delay: 5000,
-   },
+    direction: "vertical",
+    slidesPerView: "auto",
+    freeMode: true,
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        draggable: true,
+    },
+    mousewheel: true,
+    speed: 500,
+    autoplay: {
+        delay: 5000,
+    },
 
 });
-
