f1805f79c2154cba69c96f65114e3e6e99ed6938
Rombt <rombt@gmail.com>
2023-04-25 16:33:17 +0300

    fttp доступ

--- gulp/config/ftp_config.js
+++ gulp/config/ftp_config.js
@@ -1,6 +1,8 @@
 export let configFTP = {
    host: "ftpupload.net",      // адрес FTP сервера
-   user: "epiz_31905823",      // имя пользователя
-   password: "LjnwBzWINvy79H",
+   // user: "epiz_31905823",      // имя пользователя
+   // password: "LjnwBzWINvy79H",
+   user: "epiz_32063210",      // имя пользователя
+   password: "0662LvukTJKyo",
    parallel: 5    // количество одновременных потоков
 }
