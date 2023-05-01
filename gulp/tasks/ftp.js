07b7b97556e1448e222b7cf924d42da8e511b9fa
Rombt <Rombt@gmail.com>
2023-05-01 09:09:58 +0300

    что то

--- gulp/tasks/ftp.js
+++ gulp/tasks/ftp.js
@@ -7,7 +7,7 @@ export const ftp = () => {
     configFTP.log = util.log;
     const ftpConnect = vinylFTP.create(configFTP);
 
-    return app.gulp.src(app.plugins.if(app.isWP, [`${app.path.themePath}/**/*.*`, `!${app.path.themePath}/gulp/**/*.*`], `${app.path.prodFolder}/**/*.*`), {})
+    return app.gulp.src(app.plugins.if(app.isWP, [`${app.path.themePath}/**/*.*`, `!${app.path.themePath}/${app.path.rootFolder}/**/*.*`], `${app.path.prodFolder}/**/*.*`), {})
         .pipe(app.plugins.plumber(app.plugins.notify.onError({ title: "FTP", message: "Error: <%= error.message %>" })))
 
 
