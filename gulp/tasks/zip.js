817b280b8359e1fdb930c8b842ab3e5ccec95b4a
Rombt <Rombt@gmail.com>
2023-05-01 09:10:01 +0300

    что то

--- gulp/tasks/zip.js
+++ gulp/tasks/zip.js
@@ -3,7 +3,7 @@ import zipPlugin from "gulp-zip";
 
 export const zip = () => {
     del(`./${app.path.wp.themeName}.zip`);
-    return app.gulp.src(app.plugins.if(app.isWP, [`${app.path.themePath}/**/*.*`, `!${app.path.themePath}/gulp/**/*.*`], `${app.path.prodFolder}/**/*.*`), {})
+    return app.gulp.src(app.plugins.if(app.isWP, [`${app.path.themePath}/**/*.*`, `!${app.path.themePath}/${app.path.rootFolder}/**/*.*`], `${app.path.prodFolder}/**/*.*`), {})
         .pipe(app.plugins.plumber(app.plugins.notify.onError({ title: "ZIP", message: "Error: <%= error.message %>" })))
 
         .pipe(zipPlugin(`${app.path.wp.themeName}.zip`))
