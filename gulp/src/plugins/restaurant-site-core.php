ab4110c3692a2a984648d2a85b2ee2556c85de29
Rombt <Rombt@gmail.com>
2023-05-10 16:33:50 +0300

    что то

--- gulp/src/plugins/restaurant-site-core.php
+++ gulp/src/plugins/restaurant-site-core.php
@@ -1,17 +1,19 @@
-/**
-
-
-
-***************
-
-000
-
-55
-
-
-
-
-
-
-
+<?php
+/*
+Plugin Name: Restaurant-site core
+Plugin URI: #
+Description: A plugin that implements Restaurant-site theme functionality
+Version: 1.0
+Author: Automattic - Rombt
+Author URI: #
+License: GPLv2 or later
+Text Domain: restaurant-site-core
 */
+
+
+if ( !function_exists( 'add_action' ) ) {
+	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+	exit;
+}
+
+// require_once (plugin_dir_path(__FILE__ ) . '/inc/') ;
