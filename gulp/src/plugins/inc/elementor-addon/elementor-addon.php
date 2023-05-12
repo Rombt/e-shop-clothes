de80e001ad2e926525c9c32cf6eae85f9bc3ebaa
Rombt <Rombt@gmail.com>
2023-05-12 16:08:23 +0300

    что то

--- gulp/src/plugins/inc/elementor-addon/elementor-addon.php
+++ gulp/src/plugins/inc/elementor-addon/elementor-addon.php
@@ -1,18 +1,50 @@
 <?php
+
+
+
 /**
- * Plugin Name: Elementor Addon
- * Description: my test widget
+ * Plugin Name: Elementor Restaurant-site Addon
+ * Description: Elementor addon for the Restaurant-site theme.
+ * Plugin URI:  #
  * Version:     1.0.0
  * Author:      Rombt
  * Author URI:  #
- * Text Domain: restaurant-site-widget
+ * Text Domain: restaurant_site_addon
+ * 
+ * Elementor tested up to: 3.7.0
+ * Elementor Pro tested up to: 3.7.0
  */
 
-function register_hello_world_widget( $widgets_manager ) {
 
-	require_once( __DIR__ . '\includes\widgets\dish-widget.php' );
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
 
-	$widgets_manager->register( new \Elementor_Dish_Widget() );
+
+
+
+//      не работает:
+function elementor_restaurant_site_addon() {
+
+	require_once( __DIR__ . '/includes/plugin.php' );
+
+	\Elementor_Restaurant_Site_Addon\Plugin::instance();
 
 }
-add_action( 'elementor/widgets/register', 'register_hello_world_widget' );
+add_action( 'plugins_loaded', 'elementor_restaurant_site_addon' );
+
+
+
+function add_elementor_widget_categories( $elements_manager ) {
+
+	$elements_manager->add_category(
+		'restaurant-site',
+		[
+			'title' => esc_html__( 'Restaurant site', 'restaurant-site-core' ),
+			// 'icon' => 'fa fa-plug',
+			'icon' => 'eicon-code',
+		]
+	);
+
+}
+add_action( 'elementor/elements/categories_registered', 'add_elementor_widget_categories' );
