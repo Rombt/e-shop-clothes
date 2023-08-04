9df9c89861ebfb30c41b0f76e2adf90fdd60a42a
Rombt <Rombt@gmail.com>
2023-08-04 13:44:03 +0300

    start of dev ajax on menu-page

--- functions.php
+++ functions.php
@@ -144,7 +144,7 @@ function restaurant_site_scripts() {
 	wp_enqueue_style( 'restaurant_site-main', get_template_directory_uri() . '/assets/styles/main-style.min.css', array(), '1.0', 'all' );
 
 	wp_enqueue_script( 'swiper-bundle', 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js', array(), '', true );
-	wp_enqueue_script( 'restaurant_site-app', get_template_directory_uri() . '/assets/js/app.min.js', array( 'jquery' ), '1.0', true );
+	wp_enqueue_script( 'restaurant_site-app', get_template_directory_uri() . '/assets/js/app.main.min.js', array( 'jquery' ), '1.0', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
